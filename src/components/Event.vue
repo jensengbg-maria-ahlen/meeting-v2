dd4baf26c9f685d7c49583726626fade8a51e6c1
mariaahlen <59694731+mariaahlen@users.noreply.github.com>
2021-02-16 14:39:05 +0100

    added changes to vue.config

--- src/components/Event.vue
+++ src/components/Event.vue
@@ -1,9 +1,10 @@
 <template>
-    <div class="event" @click="goTo">
+    <div class="event">
         <h3 class="title">{{event.title}}</h3>
         <img :src="event.imgUrl" alt="bild">
-        <h3>{{event.when}}</h3>
-        <h3>{{event.organizer}}</h3>
+        <h3>Datum: {{event.when}}</h3>
+        <h3>Arrangör: {{event.organizer}}</h3>
+        <button @click="showInfo"> Mer info</button>
     </div>
 </template>
 
@@ -15,6 +16,9 @@ export default {
     methods: {
         goTo() {
             console.log('Hej')
+        },
+        showInfo() {
+            console.log('merInfo')
         }
     }
 }
